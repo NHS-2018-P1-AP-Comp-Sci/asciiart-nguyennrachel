32a809d1b5f43c1d1d64863992bfc6bb7f5b1d05
s-nguyenra <s-nguyenra@53S50030848.it.bsd405.org>
2018-09-20 15:19:49 -0700

    Turning in project 1

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -1,17 +1,53 @@
 /**
  * Unit 1 Project
  * 
- * In this project you have the opportunity to create a piece of artwork using ASCII 
- * (American Standard Code for Information Interchange). Using the already created file 
- * named `ASCIIArt.java` write a program that outputs your artwork to the console. 
- * The goal of this project is to use ONLY what we have learned in Unit 1 to create 
- * anything you would like so be creative (and school appropriate). Artwork must also have a caption describing what it's depicting.  
+ * In this project you have the opportunity to create a piece of artwork using
+ * ASCII (American Standard Code for Information Interchange). Using the already
+ * created file named `ASCIIArt.java` write a program that outputs your artwork
+ * to the console. The goal of this project is to use ONLY what we have learned
+ * in Unit 1 to create anything you would like so be creative (and school
+ * appropriate). Artwork must also have a caption describing what it's
+ * depicting.
  * 
- * For those of you who have dabbled in coding before, try to contain your code to 
- * just what we have discussed in this unit.  
+ * For those of you who have dabbled in coding before, try to contain your code
+ * to just what we have discussed in this unit.
  * 
- * @author Mr. Rasmussen 
+ * @author Mr. Rasmussen
  *
  */
 
 // YOUR CODE GOES HERE :)
+public class ASCIIArt {
+
+public static void main(String[] args) 
+	// TODO Auto-generated method stub
+	{
+		message3();
+		message1();
+		message2();
+		message2();
+		message2();
+		message1();
+	}
+
+public static void message1() 
+{
+	System.out.println("__________");
+}
+
+public static void message2() 
+{
+	System.out.println("|         |");
+}
+
+public static void message3()
+{
+		
+	System.out.println("    /\\");
+	System.out.println("   /  \\");
+	System.out.println("  /    \\");
+	System.out.println(" /      \\");
+			
+	}
+
+}
